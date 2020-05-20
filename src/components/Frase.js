408d8d032623b61ded10519b8d542b5558c9aa92
bbedoya <bbedoya@proteccion.com.co>
2020-05-20 17:47:24 -0500

    finished project

---
+++ src/components/Frase.js
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  padding: 3rem;
+  border-radius: 0.5rem;
+  background-color: #fff;
+  max-width: 800px;
+  @media (min-width: 992px) {
+    margin-top: 10rem;
+  }
+  h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    position: relative;
+    padding-left: 4rem;
+    &::before {
+      content: open-quote;
+      font-size: 10rem;
+      color: black;
+      position: absolute;
+      left: -1rem;
+      top: -2rem;
+    }
+  }
+  p {
+    font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+    font-size: 1.4rem;
+    text-align: right;
+    color: #666;
+    font-weight: bold;
+    margin-top: 2rem;
+  }
+`;
+
+const Frase = ({ frase }) => {
+  return Object.keys(frase).length === 0 ? null : (
+    <Container>
+      <h1>{frase.quote}</h1>
+      <p>- {frase.author}</p>
+    </Container>
+  );
+};
+
+export default Frase;
